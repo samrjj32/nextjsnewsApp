d05c2c6afbe0f0191217054b242296e910d53833
samrjj32 <samrajj3223@gmail.com>
2021-09-22 17:59:54 +0530

    working now

--- pages/employee.js
+++ pages/employee.js
@@ -3,7 +3,7 @@ import { Toolbar } from "../components/toolbar";
 
 import React from "react";
 
-export default function Employee() {
+export default function Employee({ employee }) {
   return (
     <div className="page-container">
       <Toolbar />
@@ -12,12 +12,26 @@ export default function Employee() {
         <h1>Employee Of The Month</h1>
 
         <div className={styles.employeeOfTheMonth}>
-          <h3>{"sam"}</h3>
-          <h6>{"swe"}</h6>
-          <img src={""} alt="employee" />
-          <p>{"deam big cicho"}</p>
+          <h3>{employee.name}</h3>
+          <h6>{employee.position}</h6>
+          <img src={employee.image} alt="employee" />
+          <p>{employee.description}</p>
         </div>
       </div>
     </div>
   );
 }
+
+export const getServerSideProps = async (pageContext) => {
+  const apiResponse = await fetch(
+    "https://my-json-server.typicode.com/samrjj32/nextjsnewsApp/employeeOfTheMonth"
+  );
+  const employee = await apiResponse.json();
+  
+
+  return {
+    props: {
+      employee,
+    },
+  };
+};
--- pages/feed/[id].js
+++ pages/feed/[id].js
@@ -1,14 +1,78 @@
-import { useRouter } from 'next/router';
-import styles from '../../styles/Feed.module.css';
-import { Toolbar } from "../toolbar../components/toolbar";
+import styles from "../../styles/Feed.module.css";
+import { Toolbar } from "../../components/toolbar";
+import { useRouter } from "next/router";
 
-import React from 'react'
+import React from "react";
 
-export default function Feed() {
-    return (
-        <div className="page-container">
-        <Toolbar />
-            haha feed
+export const Feed = ({ articles, pageNumber }) => {
+  const router = useRouter();
+ 
+  return (
+    <div className="page-container">
+      <Toolbar />
+      <div className={styles.main}>
+       
+        {articles.map((article, index) => (
+          <div key={index} className={styles.post}>
+            <h1 onClick={() => (window.location.href = article.url)}>
+              {article.title}
+            </h1>
+            <p>{article.description}</p>
+            {!!article.urlToImage && <img src={article.urlToImage} />}
+          </div>
+        ))}
+      </div>
+      <div className={styles.paginator}>
+          <div
+            className={pageNumber === 1 ? styles.disabled : styles.active}
+            onClick={() => {
+              if (pageNumber > 1) {
+                
+                router.push(`/feed/${pageNumber - 1}`).then(() => window.scrollTo(0, 0));
+              }
+            }}
+          >
+            Previous 
+          </div>
+
+          <div>#{pageNumber}</div>
+
+          <div
+            className={pageNumber === 5 ? styles.disabled : styles.active}
+            onClick={() => {
+              if (pageNumber < 5) {
+               
+                router.push(`/feed/${pageNumber + 1}`).then(() => window.scrollTo(0, 0));
+              }
+            }}
+          >
+            Next 
+          </div>
         </div>
-    )
-}
+      </div>
+    
+  );
+};
+
+export const getServerSideProps = async (pageContext) => {
+  const pageNumber = pageContext.query.id;
+  console.log(pageNumber,"opee")
+  const apiResponse = await fetch(
+    `https://newsapi.org/v2/top-headlines?country=us&pageSize=5&page=${pageNumber}`,
+    {
+      headers: {
+        Authorization: `Bearer ${process.env.NEXT_PUBLIC_NEWS_KEY}`,
+      },
+    }
+  ).then((res) => res.json());
+
+  const { articles } = apiResponse;
+  return {
+    props: {
+      articles: articles,
+      pageNumber: Number.parseInt(pageNumber),
+    },
+  };
+};
+
+export default Feed;
